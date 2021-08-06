ace1e352db9af0065429349972cb88de8a18d129
Eldorajo <77945458+Eldorajo@users.noreply.github.com>
2021-08-05 20:28:47 -0400

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ class Navbar extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+      <nav className="navbar navbar-dark fixed-top bg-primary flex-md-nowrap p-0 shadow">
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
           href=""
@@ -15,7 +15,8 @@ class Navbar extends Component {
         </a>
         <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-            <small className="text-white"><span id="account">{this.props.account}</span></small>
+		  <medium className="text-white"><span id="account">Current Address:   </span></medium>
+            <medium className="text-white"><span id="account">{this.props.account}</span></medium>
           </li>
         </ul>
       </nav>
